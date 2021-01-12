9e2a6c3265bb71592ddb9be8942f072e082dd0e1
Rmiverson <Rmiverson99@gmail.com>
2021-01-12 11:45:46 -0800

    done

--- index.js
+++ index.js
@@ -10,4 +10,18 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+const greet = (str) => {
+  let time = str.split(':')
+  if (time[0] < 12) {
+    return "Good Morning"
+  } else if (time[0] > 12 && time[0] < 17) {
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  }
+}
+
 /* Write your implementation of displayMessage() */
+const displayMessage = (str) => {
+  document.querySelector('#greeting').innerText = str
+}
